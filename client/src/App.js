aa8a066b893b9eaecdf3ed6cc69dab9268548ce7
Pratyush Garg <pratyushhg@gmail.com>
2021-04-25 17:11:42 +0530

    mongodb integrated for saving

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 import TextEditor from "./components/textEditor/textEditor";
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
